008333298e2b112b5f6a89164e6e90970cdc0ac8
Calvin,Yang <YangsCalvin@gmail.com>
2022-01-19 19:50:56 +0800

    Initial Commit

---
+++ ios-build-action-demo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ios-build-action-demo
+//
+//  Created by Calvin Yangs on 2022/1/19.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
